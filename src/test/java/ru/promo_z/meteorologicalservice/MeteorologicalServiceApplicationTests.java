01aa12abfdf651785c0f04e25bf4de41cbfc341a
Kirill <euchekavelo@gmail.com>
2024-10-11 06:54:25 +0300

    Добавлен первоначальный каркас сервиса для последующей разработки.

---
+++ src/test/java/ru/promo_z/meteorologicalservice/MeteorologicalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.promo_z.meteorologicalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteorologicalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
